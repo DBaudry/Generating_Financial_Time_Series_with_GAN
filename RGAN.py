3517ce2e01d971b9a5b6bb2b2b706054f71d7a95
Dorian Baudry <dobaudry19@gmail.com>
2019-04-14 16:37:14 +0200

    Run xp on each architecture

--- RGAN.py
+++ RGAN.py
@@ -3,21 +3,32 @@ from GAN import *
 
 # Define the generator
 class Generator(nn.Module):
-    def __init__(self, window=60, PRIOR_N=10, nlayers=2):
+    def __init__(self, window=60, PRIOR_N=10, PRIOR_STD=1., nlayers=2, hidden_size=20):
         super().__init__()
         self.window = window
         self.PRIOR_N = PRIOR_N
+        self.PRIOR_STD = PRIOR_STD
         self.nlayers = nlayers
-        self.rnn = nn.LSTM(PRIOR_N, window, nlayers)
+        self.hidden_size = hidden_size
+        # self.rnn = nn.LSTM(PRIOR_N, window, nlayers)
+        self.rnn = nn.LSTM(input_size=1, hidden_size=hidden_size, num_layers=nlayers)
+        self.bn = nn.BatchNorm1d(window)
+        self.fc_out = nn.Linear(hidden_size, 1)
 
     def __call__(self, batchlen, input, hx, cx):
         output, (hx, cx) = self.rnn(input, (hx, cx))
-        return output[:, 0, :]
+        output = output.transpose(0, 1)
+        return self.fc_out(output)[:, :, 0]
 
     def generate(self, batchlen):
-        input = torch.randn(batchlen, 1, self.PRIOR_N)
-        hx = torch.randn(self.nlayers, 1, self.window)
-        cx = torch.randn(self.nlayers, 1, self.window)
+        # input = torch.randn(batchlen, 1, self.PRIOR_N)
+        # hx = torch.randn(self.nlayers, 1, self.window)
+        # cx = torch.randn(self.nlayers, 1, self.window)
+        input = torch.normal(torch.zeros(self.PRIOR_N, batchlen, 1), self.PRIOR_STD)
+        # hx = torch.normal(torch.zeros(self.nlayers, batchlen, self.hidden_size), self.PRIOR_STD)
+        # cx = torch.normal(torch.zeros(self.nlayers, batchlen, self.hidden_size), self.PRIOR_STD)
+        hx = torch.zeros(self.nlayers, batchlen, self.hidden_size)
+        cx = torch.zeros(self.nlayers, batchlen, self.hidden_size)
         return self.__call__(batchlen, input, hx, cx)
 
 
@@ -26,19 +37,46 @@ class Discriminator(nn.Module):
     def __init__(self, window, nlayers):
         super().__init__()
         self.nlayers = nlayers
-        self.rnn = nn.LSTM(window, window, nlayers)
+        # self.rnn = nn.LSTM(window, window, nlayers)
+        self.rnn = nn.LSTM(input_size=1, hidden_size=20, num_layers=nlayers, batch_first=True)
+        self.fc_out = nn.Linear(20, 1)
 
     def __call__(self, x):
-        input = x.unsqueeze(1)
+        input = x.unsqueeze(2)
         output, (hx, cx) = self.rnn(input)
-        return torch.sigmoid(output[:, 0, :])
+        output = self.fc_out(output)[:, :, 0]
+        return torch.sigmoid(output)
+
+
+def random_xp(n_xp):
+    window = [60, 125, 250]
+    PRIOR_N = [60]
+    Train_ratio = [1, 3, 5, 10, 20, 50]
+    PRIOR_STD = [1., 10, 20, 100]
+    N_LAYERS = [1, 2, 3, 5, 10]
+    for i in range(n_xp):
+        param['window'] = np.random.choice(window)
+        param['generator_args']['PRIOR_N'] = np.random.choice(PRIOR_N)
+        param['generator_args']['PRIOR_STD'] = np.random.choice(PRIOR_STD)
+        param['TRAIN_RATIO'] = np.random.choice(Train_ratio)
+        param['generator_args']['nlayers'] = np.random.choice(N_LAYERS)
+        param['discriminator_args']['nlayers'] = np.random.choice(N_LAYERS)
+        param['save_name'] = 'RGAN_'+str(int(np.random.uniform()*1e9))
+        print('Iteration %f' % i)
+        print((param['window'], param['TRAIN_RATIO']))
+        print(param['generator_args'])
+        print(param['discriminator_args'])
+        if param['save_model']:
+            pickle.dump(param, open('Parameters/'+param['save_name']+'.pk', 'wb'))
+        GAN(**param)
 
 
 if __name__ == '__main__':
     param = {
         'serie': get_data('VIX.csv'),
         'window': 60,
-        'frame': 2000,
+        'frame': 20,
+        'frame_plot': 20,
         'is_notebook': False,
         'batchlen_plot': 5,
         'Generator': Generator,
@@ -46,12 +84,14 @@ if __name__ == '__main__':
         'BATCHLEN': 30
     }
     training_param = {
-        'N_ITER': 2001,
+        'N_ITER': 20001,
         'TRAIN_RATIO': 5,
         # Random Noise used by the Generator
         'generator_args': {
-            'PRIOR_N': 1,
+            'PRIOR_N': 60,
+            'PRIOR_STD': 10.,
             'nlayers': 1,
+            'hidden_size': 1
         },
         # Depth and Withdraw of Hidden Layers
         'discriminator_args': {
@@ -64,9 +104,17 @@ if __name__ == '__main__':
         'loss': utils.negative_cross_entropy,
         'argloss_real': torch.ones(param['BATCHLEN'], dtype=torch.int64),
         'argloss_fake': torch.zeros(param['BATCHLEN'], dtype=torch.int64),
-        'argloss_gen': torch.ones(param['BATCHLEN'], dtype=torch.int64)
+        'argloss_gen': torch.ones(param['BATCHLEN'], dtype=torch.int64),
+        'save_model': True,
+        'save_name': 'RGAN_'+str(int(np.random.uniform()*1e9)),
+        'plot': True,
+        'time_max': 1200
     }
 
     param.update(training_param)
+
+    # random_xp(100)
+    # if param['save_model']:
+    #     pickle.dump(param, open('Parameters/'+param['save_name']+'.pk', 'wb'))
     GAN(**param)
 
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import pickle
+from scipy.stats import gaussian_kde, entropy
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 def get_data(name, array=True):
@@ -16,14 +19,20 @@ def get_data(name, array=True):
     return r
 
 
-def generate_batch(serie, length, BATCHLEN):
+def generate_batch(serie, length, BATCHLEN, add_noise=True, noise_lvl=0.2, proba_noise=0.5):
     """
     Returns random sequences of the given length from a numpy array
     """
+    if add_noise:
+        sd = np.std(serie)
+        sigma = noise_lvl * sd
     results = np.zeros((BATCHLEN, length))
     for i in range(BATCHLEN):
         random_start = np.random.choice(serie.shape[0]-length)
+        u = np.random.binomial(n=1, p=proba_noise)
         results[i] = serie[random_start: random_start+length]
+        if add_noise:
+            results[i] += u*sigma*np.random.normal(size=length)
     return torch.tensor(results).float()
 
 
@@ -32,7 +41,47 @@ def softplus_loss(h, sgn):
 
 
 def negative_cross_entropy(h, target):
-    return torch.mean(torch.sum(-F.cross_entropy(h, target)))
+    return torch.mean(torch.sum(F.cross_entropy(h, target)))
+
+
+def KL_div(Gen, serie, length, batchlen):
+    real_batch = generate_batch(serie, length, batchlen).detach().numpy().flatten()
+    fake_batch = Gen.generate(batchlen).detach().numpy().flatten()
+    r_kde = gaussian_kde(real_batch)
+    f_kde = gaussian_kde(fake_batch)
+    eval_points = np.linspace(-6, 6, 500)
+    pdf_values_real = r_kde.pdf(eval_points)
+    pdf_values_fake = f_kde.pdf(eval_points)
+    kl_div = entropy(pdf_values_real, pdf_values_fake)
+    return kl_div
+
+
+def get_KL_div_list(data, label, batchlen, name_wdw, gen, disc):
+    corresp = {'3M': 60, '6M': 125, 'Y': 250}
+    name_order = []
+    KL_div_list = []
+    for name in tqdm(name_wdw[label]):
+        G, D, param_name = load_models(name, gen, disc)
+        name_order.append(name)
+        KL_div_list.append(KL_div(G, data, corresp[label], batchlen))
+    return name_order, KL_div_list
+
+
+def compare_plots(name, serie, batchlen, window, Gen, Disc):
+    G, D, param_name = load_models(name, Gen, Disc)
+    print(param_name)
+    real_batch = (generate_batch(serie, window, batchlen)-np.mean(serie))/np.std(serie)
+    fake_batch = G.generate(batchlen).detach()
+    random_batch = np.random.normal(size=(window, batchlen)) * np.std(serie)
+    fig, ax = plt.subplots(3, figsize=(20, 20), sharey=True)
+    fig.suptitle('Real Batch vs Generated Batch and Random Batch')
+    ax[0].plot(real_batch.numpy().T)
+    ax[0].set_title('Batch from Real Serie')
+    ax[1].plot(fake_batch.numpy().T)
+    ax[1].set_title('Generated Batch')
+    ax[2].plot(random_batch)
+    ax[2].set_title('Random Batch with the Standard deviation of the original serie')
+    plt.show()
 
 
 def load_models(name, Generator, Discriminator):
